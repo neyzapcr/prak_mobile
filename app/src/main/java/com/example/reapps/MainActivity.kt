7a14f0190fead3e4b7e71715b3aac87bb69c7edc
Neyza <lab151-29@Lab151-29s-iMac.local>
2026-04-01 08:35:45 +0700

    penerapan view binding  atribut 01 April 08.35

--- app/src/main/java/com/example/reapps/MainActivity.kt
+++ app/src/main/java/com/example/reapps/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.reapps
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
--- app/src/main/java/com/example/reapps/pertemuan_3/ThirdActivity.kt
+++ app/src/main/java/com/example/reapps/pertemuan_3/ThirdActivity.kt
@@ -1,21 +1,40 @@
 package com.example.reapps.pertemuan_3
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.reapps.R
+import com.example.reapps.databinding.ActivityThirdBinding
 
 class ThirdActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityThirdBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         setContentView(R.layout.activity_third)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+//        val inputNoTujuan: EditText = findViewById(R.id.inputNoTujuan)
+//        val btnKirim: Button = findViewById(R.id.btnKirim)
+
+        binding.btnKirim.setOnClickListener {
+            val noTujuan = binding.inputNoTujuan.text
+            Toast.makeText(this, "Pesan berhasil dikirim ke $noTujuan", Toast.LENGTH_SHORT).show()
         }
     }
 }
