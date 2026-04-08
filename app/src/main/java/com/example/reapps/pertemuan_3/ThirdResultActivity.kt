822786091b794db510b03a2980ae789fac7309c9
Neyza <lab151-29@Lab151-29s-iMac.local>
2026-04-08 07:10:03 +0700

    tes p4

--- app/src/main/java/com/example/reapps/pertemuan_3/ThirdResultActivity.kt
+++ app/src/main/java/com/example/reapps/pertemuan_3/ThirdResultActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.reapps.R
-
+//
 class ThirdResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
