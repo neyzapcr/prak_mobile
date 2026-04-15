05f643b9f7409b287a0459f197b097866e67f825
neyza <neyza24si@mahasiswa.pcr.ac.id>
2026-04-15 11:43:27 +0700

    pratikum online p5 15/04/2026

---
+++ app/src/main/java/com/example/reapps/pertemuan_5/WebViewActivity.kt
@@ -0,0 +1,51 @@
+package com.example.reapps.pertemuan_5
+
+import android.os.Bundle
+import android.webkit.WebViewClient
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.reapps.R
+import com.example.reapps.databinding.ActivityWebViewBinding
+
+class WebViewActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWebViewBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWebViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Mengaktifkan toolbar
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.apply {
+            title = "Web Merdeka"
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
+
+        binding.webView.webViewClient = WebViewClient()
+        binding.webView.settings.javaScriptEnabled = true
+        binding.webView.loadUrl("https://merdeka.com")
+
+        // Agar Toolbar hide/show saat scroll web
+        binding.webView.setOnScrollChangeListener { _, _, scrollY, _, oldScrollY ->
+            if (scrollY > oldScrollY) {
+                binding.appBar.setExpanded(false, true)
+            } else if (scrollY < oldScrollY) {
+                binding.appBar.setExpanded(true, true)
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (binding.webView.canGoBack()) {
+            binding.webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
+
