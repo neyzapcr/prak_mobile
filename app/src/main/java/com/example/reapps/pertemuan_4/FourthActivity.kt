7a2835bd6762e41042b61a8c239de944d04aeb53
Neyza <lab151-29@Lab151-29s-iMac.local>
2026-04-22 07:20:59 +0700

    p6 test

--- app/src/main/java/com/example/reapps/pertemuan_4/FourthActivity.kt
+++ app/src/main/java/com/example/reapps/pertemuan_4/FourthActivity.kt
@@ -8,7 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.reapps.MainActivity
+//import com.example.reapps.MainActivity
 import com.example.reapps.R
 import com.example.reapps.databinding.ActivityFourthBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
