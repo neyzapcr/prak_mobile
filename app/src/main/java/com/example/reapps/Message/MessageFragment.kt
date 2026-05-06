063e67589ddf26f923f51525e2c18296772bae70
Neyza <lab151-29@Lab151-29s-iMac.local>
2026-05-06 07:02:53 +0700

    p8 tes

--- app/src/main/java/com/example/reapps/Message/MessageFragment.kt
+++ app/src/main/java/com/example/reapps/Message/MessageFragment.kt
@@ -19,7 +19,6 @@ class MessageFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         _binding = FragmentMessageBinding.inflate(inflater, container, false)
         return binding.root
     }
